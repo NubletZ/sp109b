b5c0ac617bab5a8dd68d71a0bfe42d890db4d6e0
NubletZ <wpandita2001@gmail.com>
2021-06-11 00:49:02 +0800

    add vertical scroll function

--- TeDi(TextEditor)/tedi.c
+++ TeDi(TextEditor)/tedi.c
@@ -1,3 +1,7 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -5,6 +9,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <string.h>
 
 /*** defines ***/
@@ -31,10 +36,19 @@ enum moveKey {
 
 /*** data ***/
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
+  int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
+  int numrows;
+  erow *row; //define new name for struct erow
   struct termios orig_termios;
 };
 
@@ -180,6 +194,38 @@ unlike H command C and B won't let cursor to past the edge of screen
   }
 }
 
+/*** row operations ***/
+
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
+/*** file i/o ***/
+
+void editorOpen(char *filename) {
+//used for opening and reading a file from disk
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
+
+    editorAppendRow(line, linelen);
+  }
+  free(line);
+  fclose(fp);
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -202,24 +248,42 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
+
 void editorDrawRows(struct abuf *ab) {
 //draw tildes (~) at the begining of each line
   int y;
-  for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome), "TeDi(Text Editor) -- version %s", TEDI_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
-        abAppend(ab, "~", 1);
-        padding--;
+  for (y = 0; y < E.screenrows - 1; y++) {
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) { //show welcome message (only when TeDi is empty)
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome), "TeDi(Text Editor) -- version %s", TEDI_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) { //if(int) same with if(int =! 0)
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
       }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
+      else {
+        abAppend(ab, "~", 1);
+      }
     }
     else {
-      abAppend(ab, "~", 1);
+      int len = E.row[filerow].size;
+      if (len < 0) len = 0;
+      if ( len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -229,6 +293,9 @@ void editorDrawRows(struct abuf *ab) {
 3. \x1b[ : escape sequances to instruct terminal to do various text formatting tasks including clearing screen
 4. K : erase part of the current line
 */
+    if (y == E.screenrows - 1) {
+      abAppend(ab, "hello", 5);
+    }
 
     if (y < E.screenrows - 1) {
       //only write "\r\n" until before end of line
@@ -238,6 +305,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -250,7 +319,7 @@ H command is used to position the cursor, in the line above we set cursor positi
   editorDrawRows(&ab); //pass ab value so it can be use in editorDrawRows()
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy -E.rowoff) + 1, E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -273,7 +342,7 @@ void editorMoveCursor(int key) {
       if (E.cy != 0) E.cy--;
       break;
     case ARROW_DOWN:
-      if (E.cy != E.screenrows - 1) E.cy++;
+      if (E.cy < E.numrows) E.cy++;
       break;
   }
 }
@@ -318,18 +387,26 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0; //horizontal coordinate of cursor
   E.cy = 0; //vertical coordinate of cursor
+  E.rowoff = 0;
+  E.coloff = 0; 
+  E.numrows = 0;
+  E.row = NULL;
 
 //initialize screenrows and screencols in E struct
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
   }
+
   return 0;
 }
