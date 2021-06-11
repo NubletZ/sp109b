a8fb69e72810d321da8ace779caea518e6dbecd9
NubletZ <wpandita2001@gmail.com>
2021-06-12 06:05:36 +0800

    add render (cursor position if there is any tabs), add message time

--- TeDi(TextEditor)/tedi.c
+++ TeDi(TextEditor)/tedi.c
@@ -11,13 +11,16 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <string.h>
+#include <time.h>
+#include <stdarg.h>
 
 /*** defines ***/
 
-#define CTRL_KEY(k) (27)
+#define CTRL_KEY(k) ((k) & 0x1f)
 #define ESC 27
 #define ABUF_INIT {NULL, 0}
 #define TEDI_VERSION "0.1"
+#define TEDI_TAB_STOP 8
 
 enum moveKey {
 /* -- COMMENT --
@@ -39,11 +42,14 @@ enum moveKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
   int cx, cy;
+  int rx; //cursor position if there is any tabs
   int rowoff;
   int coloff;
   int screenrows;
@@ -51,8 +57,10 @@ struct editorConfig {
   int numrows;
   erow *row; //define new name for struct erow
   int escstat; //flag if user press esc
+  int esc27; //flag to devide between esc key press and esc return
   char message[80]; //define message at the bottom
-  char filename[10];
+  time_t message_time;
+  char *filename;
   struct termios orig_termios;
 };
 
@@ -114,15 +122,28 @@ int editorReadKey() {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
 
+  //sprintf(E.message, "c = %c", c);
+
   if (c == '\x1b') {
     char seq[3];
 
-    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
-    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      //sprintf(E.message, "enter first c=%d", c);
+      E.esc27 = 1;
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      //sprintf(E.message, "enter second");
+      return '\x1b';
+    }
+    //sprintf(E.message, "test = %c %c", seq[0], seq[1]);
 
     if (seq[0] == '[') {
       if (seq[1] >= '0' && seq[1] <= '9') {
-        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) {
+          //sprintf(E.message, "enter third");
+          return '\x1b';
+        }
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
@@ -198,16 +219,82 @@ unlike H command C and B won't let cursor to past the edge of screen
   }
 }
 
+/*** message below ***/
+
+void setMessage(int type, char *string) {
+  if (type == 1) {
+    snprintf(E.message, sizeof(E.message), "-- EDIT TEXT | %.20s --", string);
+  }
+  if (type == 27) {
+    sprintf(E.message, "> Do you realy want to exit TeDi? (y/n)");
+  }
+  E.message_time = time(NULL);
+}
+
+void messageSaveTime() {
+  if (E.escstat != 1) {
+    int msglen = strlen(E.message);
+    if (msglen && time(NULL) - E.message_time < 5) {
+      setMessage(1, E.filename);
+    }
+  }
+}
+
 /*** row operations ***/
+
+int editorRowCxToRx(erow *row, int cx) {
+//calculate the value of E.rx
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t') {
+      rx += (TEDI_TAB_STOP - 1) - (rx % TEDI_TAB_STOP);
+    }
+    rx++;
+  }
+  return rx;
+}
+
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') tabs++;
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + tabs*(TEDI_TAB_STOP - 1) + 1);
+/* -- COMMENT --
+The maximum number of characters needed for each tab is 8
+*/
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % TEDI_TAB_STOP != 0) row->render[idx++] = ' ';
+    }
+    else {
+      row->render[idx++] = row->chars[j]; //copy chars into render
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
 
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
+  //save each line data
   int at = E.numrows;
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -215,6 +302,10 @@ void editorAppendRow(char *s, size_t len) {
 
 void editorOpen(char *filename) {
 //used for opening and reading a file from disk
+  free(E.filename);
+  E.filename = strdup(filename);
+  setMessage(1, E.filename);
+
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
 
@@ -253,11 +344,21 @@ void abFree(struct abuf *ab) {
 /*** output ***/
 
 void editorScroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows - 1) {
     E.rowoff = E.cy - E.screenrows + 2;
+  }
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
+  }
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -284,10 +385,10 @@ void editorDrawRows(struct abuf *ab) {
       }
     }
     else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].rsize - E.coloff;
       if (len < 0) len = 0;
       if ( len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -308,7 +409,7 @@ void editorDrawRows(struct abuf *ab) {
     int messagelen = strlen(E.message);
     int blank = (E.screencols - messagelen - 1);
     abAppend(ab, E.message, messagelen);
-    while (blank--) abAppend(ab, "=", 1);
+    while (blank--) abAppend(ab, " ", 1);
   }
 }
 
@@ -325,9 +426,10 @@ H command is used to position the cursor, in the line above we set cursor positi
 */
 
   editorDrawRows(&ab); //pass ab value so it can be use in editorDrawRows()
+  messageSaveTime();
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy -E.rowoff + 1), E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy -E.rowoff) + 1, (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -339,12 +441,24 @@ H command is used to position the cursor, in the line above we set cursor positi
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) E.cx--;
+      else if (E.cy > 0) {//go to end of previouse line when E.cx == 0 and key press <
+        E.cy--;
+        E.cx = E.row[E.cy].size;
+      }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols -1) E.cx++;
+      if (row && E.cx < row->size) {
+        E.cx++;
+      }
+      else if (E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) E.cy--;
@@ -353,17 +467,27 @@ void editorMoveCursor(int key) {
       if (E.cy < E.numrows - 1) E.cy++;
       break;
   }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen; //return E.cx value when it pass rowlen value
+  }
 }
 
 void editorProcessKeypress() {
   int c = editorReadKey();
 
+  if (E.esc27 == 1) {
+    switch (c) {
+      case CTRL_KEY(ESC):
+        setMessage(27, NULL);
+        E.escstat = 1;
+        E.esc27 = 0;
+        break;
+    }
+  }
   switch (c) {
-    case ESC:
-      sprintf(E.message, "> Do you realy want to exit TeDi? (y/n)");
-      E.escstat = 1;
-      break;
-
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
@@ -375,13 +499,23 @@ void editorProcessKeypress() {
       E.cx = 0;
       break;
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows) {
+        E.cx = E.row[E.cy].size;
+      }
       break;
 
     case PAGE_UP:
-    case PAGE_DOWN:
+    case PAGE_DOWN: //next and prev page
       {
-        int times = E.screenrows;
+        if ( c == PAGE_UP) {
+          E.cy = E.rowoff;
+        }
+        else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 2;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
+
+        int times = E.screenrows - 2;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
       }
@@ -395,7 +529,7 @@ void editorProcessKeypress() {
         exit(0);
         break;
       case 'n':
-        sprintf(E.message, "-- EDIT TEXT | %s --", E.filename);
+        setMessage(1, E.filename);
         E.escstat = 0;
         break;
     }
@@ -404,16 +538,18 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
-void initEditor(char filename[50]) {
-  sprintf(E.filename, "%s", filename);
+void initEditor() {
+  E.filename = NULL;
+  E.message_time = 0;
   E.cx = 0; //horizontal coordinate of cursor
   E.cy = 0; //vertical coordinate of cursor
+  E.rx = 0; //coordinate of cursor when there is any tabs
   E.rowoff = 0;
   E.coloff = 0; 
   E.numrows = 0;
   E.row = NULL;
   E.escstat = 0;
-  sprintf(E.message, "-- EDIT TEXT | %s --", filename);
+  E.esc27 = 0;
 
 //initialize screenrows and screencols in E struct
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
@@ -421,10 +557,11 @@ void initEditor(char filename[50]) {
 
 int main(int argc, char *argv[]) {
   enableRawMode();
-  initEditor(argv[1]);
+  initEditor();
   if (argc >= 2) {
     editorOpen(argv[1]);
   }
+  else sprintf(E.message, "-- EDIT TEXT | untitled --");
 
   while (1) {
     editorRefreshScreen();
