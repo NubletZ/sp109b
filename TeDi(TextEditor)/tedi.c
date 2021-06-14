b5b6494374971c2aa3055d272ecaa01c9bab1f89
NubletZ <wpandita2001@gmail.com>
2021-06-15 01:42:05 +0800

    adding delete char function

--- TeDi(TextEditor)/tedi.c
+++ TeDi(TextEditor)/tedi.c
@@ -233,6 +233,9 @@ void setMessage(int type, const char *string, ...) {
   else if (type == 27) {
     sprintf(E.message, "> Do you realy want to exit TeDi? (y/n)");
   }
+  else if (type == 28) {
+    sprintf(E.message, "> WARNING! File has unsaved changes. Do you realy want to exit TeDi? (y/n)");
+  }
   else vsnprintf(E.message, sizeof(E.message), string, ap);
   va_end(ap);
   E.message_time = time(NULL);
@@ -288,11 +291,13 @@ The maximum number of characters needed for each tab is 8
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
+
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
   //save each line data
-  int at = E.numrows;
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -306,6 +311,19 @@ void editorAppendRow(char *s, size_t len) {
   E.dirty++;
 }
 
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
+}
+
 void editorRowInsertChar(erow *row, int at, int c) {
   if (at < 0 || at > row->size) at = row->size;
   row->chars = realloc(row->chars, row->size + 2);
@@ -316,14 +334,63 @@ void editorRowInsertChar(erow *row, int at, int c) {
   E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at); //used to overwrite the deleted character with the character that come after it
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 /*** editor operations ***/
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  }
+  else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
+  else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
 }
 
 /*** file i/o ***/
@@ -364,7 +431,7 @@ void editorOpen(char *filename) {
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
 
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -523,7 +590,7 @@ void editorMoveCursor(int key) {
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) E.cx--;
-      else if (E.cy > 0) {//go to end of previouse line when E.cx == 0 and key press <
+      else if (E.cy > 0) {//go to end of previous line when E.cx == 0 and key press <
         setMessage(1, NULL);
         E.cy--;
         E.cx = E.row[E.cy].size;
@@ -569,7 +636,10 @@ void editorProcessKeypress() {
   if (E.esc27 == 1) {
     switch (c) {
       case CTRL_KEY(ESC):
-        setMessage(27, NULL);
+        if (E.dirty == 0) {
+          setMessage(27, NULL);
+        }
+        else setMessage(28, NULL);
         E.escstat = 1;
         E.esc27 = 0;
         break;
@@ -591,7 +661,7 @@ void editorProcessKeypress() {
   else if (!E.escstat) {
     switch (c) {
       case '\r':
-        /* TODO */
+        editorInsertNewline();
         break;
 
       case CTRL_KEY('s'):
@@ -617,7 +687,8 @@ void editorProcessKeypress() {
       case BACKSPACE:
       case CTRL_KEY('h'):
       case DEL_KEY:
-        /* TODO */
+        if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+        editorDelChar();
         break;
 
       case PAGE_UP:
